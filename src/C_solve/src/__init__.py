b0693d29ea3a53a1aa52b23b1cda05ecd36b2373
吉安 季 <1120939526@qq.com>
2024-02-02 10:37:23 +0800

    update data to the python sourc

--- src/C_solve/src/__init__.py
+++ src/C_solve/src/__init__.py
@@ -10,3 +10,8 @@ map = {}
 def load_data():
     data = pd.read_csv('statics/data_dictionary.csv')
     return data
+
+def read_data(data_list):
+    data = pd.read_csv('statics/data.csv')
+
+    return data
