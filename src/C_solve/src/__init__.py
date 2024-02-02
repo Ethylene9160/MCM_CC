2f5d9c81d679722b23cdc7c85793a7a370abbb81
吉安 季 <1120939526@qq.com>
2024-02-02 11:03:26 +0800

    read data to map.

--- src/C_solve/src/__init__.py
+++ src/C_solve/src/__init__.py
@@ -4,14 +4,35 @@ import matplotlib.pyplot as plt
 
 import python.PCA as mPCA
 import python.SVM as mSVM
+from python.data_reader import hint
 
-map = {}
+data_path = 'statics/Wimbledon_featured_matches.csv'
 
-def load_data():
-    data = pd.read_csv('statics/data_dictionary.csv')
+
+def read_data(csv_file_path):
+    # 从CSV文件中读取数据
+    data = pd.read_csv(csv_file_path)
     return data
 
-def read_data(data_list):
-    data = pd.read_csv('statics/data.csv')
+def read_single_data(single_line):
+    # 将单行数据转换为字典
+    single_map = {}
+    for i, value in enumerate(single_line):
+        single_map[hint[i]] = value
+    return single_map
 
-    return data
+
+if __name__ == '__main__':
+    # 加载CSV数据
+    data = read_data(data_path)
+
+    # 直接将DataFrame中的每行转换为字典并添加到playerList
+    player_list = []
+    for index, row in data.iterrows():
+        player_data = row.to_dict()
+        player_list.append(player_data)
+
+    # 打印playerList中的前几个条目以进行验证
+    for player in player_list[:2]:
+        print(player)
+
