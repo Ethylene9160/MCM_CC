fcff7e7c40374a8cd9f644048b675e48a0d9f155
吉安 季 <1120939526@qq.com>
2024-02-03 15:21:14 +0800

    add judging methods in MCModel.py

--- src/C_solve/src/ethy/model/MCModel.py
+++ src/C_solve/src/ethy/model/MCModel.py
@@ -14,3 +14,19 @@ class MCModel:
 
     def MSELoss(self, y, y_pred):
         return np.mean(np.square(y - y_pred))
+
+    def accuracy(self, y, y_pred):
+        return np.mean(np.equal(y, y_pred))
+
+    def F1_score(self, y, y_pred):
+        TP = np.sum(np.logical_and(y_pred == 1, y == 1))
+        FP = np.sum(np.logical_and(y_pred == 1, y == 0))
+        FN = np.sum(np.logical_and(y_pred == 0, y == 1))
+        precision = TP / (TP + FP)
+        recall = TP / (TP + FN)
+        return 2 * precision * recall / (precision + recall)
+
+    def precision(self, y, y_pred):
+        TP = np.sum(np.logical_and(y_pred == 1, y == 1))
+        FP = np.sum(np.logical_and(y_pred == 1, y == 0))
+        return TP / (TP + FP)
