b44270ada32974a4750deed5a2b16fd1f13ae806
吉安 季 <1120939526@qq.com>
2024-02-03 17:47:58 +0800

    update model

--- src/C_solve/src/ethy/model/MCModel.py
+++ src/C_solve/src/ethy/model/MCModel.py
@@ -19,43 +19,6 @@ class MCModel:
             y_pred = np.array(y_pred)
         return np.mean(np.square(y - y_pred))
 
-    # def accuracy(self, y, y_pred):
-    #     if not isinstance(y, np.ndarray):
-    #         y = np.array(y)
-    #     if not isinstance(y_pred, np.ndarray):
-    #         y_pred = np.array(y_pred)
-    #     return np.mean(np.equal(y, y_pred))
-    #
-    # def F1_score(self, y, y_pred):
-    #     if not isinstance(y, np.ndarray):
-    #         y = np.array(y)
-    #     if not isinstance(y_pred, np.ndarray):
-    #         y_pred = np.array(y_pred)
-    #     TP = np.sum(np.logical_and(y_pred == 1, y == 1))
-    #     FP = np.sum(np.logical_and(y_pred == 1, y == 0))
-    #     FN = np.sum(np.logical_and(y_pred == 0, y == 1))
-    #     precision = TP / (TP + FP)
-    #     recall = TP / (TP + FN)
-    #     return 2 * precision * recall / (precision + recall)
-    #
-    # def precision(self, y, y_pred):
-    #     if not isinstance(y, np.ndarray):
-    #         y = np.array(y)
-    #     if not isinstance(y_pred, np.ndarray):
-    #         y_pred = np.array(y_pred)
-    #     TP = np.sum(np.logical_and(y_pred == 1, y == 1))
-    #     FP = np.sum(np.logical_and(y_pred == 1, y == 0))
-    #     return TP / (TP + FP)
-    #
-    # def recall(self, y, y_pred):
-    #     if not isinstance(y, np.ndarray):
-    #         y = np.array(y)
-    #     if not isinstance(y_pred, np.ndarray):
-    #         y_pred = np.array(y_pred)
-    #     TP = np.sum(np.logical_and(y_pred == 1, y == 1))
-    #     FN = np.sum(np.logical_and(y_pred == 0, y == 1))
-    #     return TP / (TP + FN)
-
     def judge(self, y, y_pred):
         if not isinstance(y, np.ndarray):
             y = np.array(y)
